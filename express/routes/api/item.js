b77381b7a35f00b52e16167f5496eaba25c2efc0
Phiraphat Kakerd <Phiraphat.zim@mail.kmutt.ac.th>
2023-03-10 12:59:43 +0700

    edit : api item.js :
    - ปรับปรุงการ get all ,by id

--- express/routes/api/item.js
+++ express/routes/api/item.js
@@ -12,26 +12,31 @@ const table='item'
 router.get('/',async(req,res)=>{
 
     try {
-        connMSQL.connection.query(
-            'SELECT * FROM moral_it_device.item',
-            (err,results)=>{
-                if(err){
-                    console.log(err)
-                    return res.status(400).send()
+        if(!connMSQL.handdleConnection()){
+            connMSQL.connection.query(
+                `SELECT * FROM moral_it_device.${table}`,
+                (err,results)=>{
+                    if(err){
+                        console.log(err)
+                        throw new Error(`Query ${table} error : `,err)
+                    }
+                    res.status(200).json(results)
                 }
-                return res.status(200).json(results)
-            }
-        )
+            )
+        }else{
+            console.log(`Cannot connect to mysql server !!`) 
+            throw new Error('connection error something :',err)
+        }
     } catch (error) {
         console.log(error)
-        return res.status(500).send()
+        return res.status(500).json(errorModel(error.message,req.originalUrl))
     }
 })
 
 
 // get item by id
 router.get('/:id',async(req,res)=>{
-    let testing
+
     try {
         if(!connMSQL.handdleConnection()){
              connMSQL.connection.query(
@@ -42,12 +47,12 @@ router.get('/:id',async(req,res)=>{
                 (err,results)=>{
                     if(err){
                         console.log(err)
-                        throw new Error('query err somethig :',err)
+                        throw new Error(`find ${table} by id err :`,err)
                     }
 
                     if(results.length==0){
-                        console.log("user id " + req.params.id + " does not exist")
-                        res.status(404).json(errorModel("user id :" + req.params.id + " does not exist",req.originalUrl))
+                        console.log(`${table} id  ${req.params.id} does not exist`)
+                        res.status(404).json(errorModel(`${table} id  ${req.params.id} does not exist`,req.originalUrl))
                     }else{
                         res.status(200).json(results)
                     }
@@ -58,11 +63,13 @@ router.get('/:id',async(req,res)=>{
             throw new Error('connection error something :',err)
         } 
     } catch (error) {
-        res.status(400).json(errorModel(error.message,req.originalUrl))
+        res.status(500).json(errorModel(error.message,req.originalUrl))
     }
 
-    console.log( testing)
 })
+
+
+
 
 
 
