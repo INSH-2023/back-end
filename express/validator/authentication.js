843cebfe69b9680803e284ffe2dd318fa2a293d1
Sahathat Yingsakulkiet <61311677+sahathat@users.noreply.github.com>
2023-04-04 16:31:41 +0700

    edit : validator authen :
    - ปรับปรุงให้ check ค่า params ก่อนใช้ connection poll

--- express/validator/authentication.js
+++ express/validator/authentication.js
@@ -4,23 +4,33 @@ const logIn=async(req)=>{
     let status =undefined
     let return_data=[]
     let {email:email,password:password}= req.body
-    
-    let statement =`SELECT userId,user_emp_code,user_email,user_role,user_password FROM moral_it_device.user WHERE user_email="${email}"`
+
+    if(email==undefined||email==null||
+        password==null||password==undefined
+    ){
+        return false
+    }
+
+    let statement =`SELECT userId,user_emp_code,user_first_name,user_last_name,user_email,user_role,user_password FROM moral_it_device.user WHERE user_email="${email}"`
 
     let sqlD={}
     let {status_pool:status_p,data:authen,msg:msg}=await connMSQL.connection_pool(statement)
+    
     if(status_p){
         console.log(authen)
-        let [{userId,user_emp_code,user_email,user_role,user_password}]=authen
+        let [{userId,user_emp_code,user_first_name,user_last_name,user_email,user_role,user_password}]=authen
         console.log('from sql :', sqlD)
         // console.log(`data : ${email} / ${password}`)
 
-        if(sqlD==null||sqlD==undefined){
-        status = false
+        if(sqlD==null||sqlD==undefined|| user_email==undefined||
+            user_email==null||user_password==null||
+            user_password==undefined
+        ){
+            status = false
         }else{
         if(user_email.toLowerCase().trim()==email.toLowerCase().trim() && user_password==password){
             status=true
-            sqlD={userId:userId,user_emp_code,user_email, user_role:user_role}
+            sqlD={userId:userId,user_emp_code,user_first_name,user_last_name, user_role:user_role}
         }else{
             status = false
         }
