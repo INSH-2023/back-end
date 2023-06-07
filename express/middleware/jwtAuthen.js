4720e1908e25c62f1d9aae0fcf19b7798a7dc958
Sahathat Yingsakulkiet <61311677+sahathat@users.noreply.github.com>
2023-06-07 09:22:35 +0700

    fix bug authentication validation

--- express/middleware/jwtAuthen.js
+++ express/middleware/jwtAuthen.js
@@ -12,7 +12,8 @@ exports.JwtAuth = (req, res, next) => {
   const jwtToken = req.headers.authorization || "Bearer " + req.cookies.token ;
   const jwtRefreshToken = req.headers.refresh || "Bearer " + req.cookies.refreshToken ;
   // ตรวจสอบถ้าไม่มี token จะเข้าสู่ระบบไม่ได้
-  if (jwtToken == "Bearer " && jwtRefreshToken == "Bearer ") return res.status(401).json(errorModel("need login first",req.originalUrl))
+  if (["null","undefined"].includes(jwtToken.substring(7)) && 
+  ["null","undefined"].includes(jwtRefreshToken.substring(7))) return res.status(401).json(errorModel("need login first",req.originalUrl))
   try {
     // ตรวจสอบ user ใน access token 
     let user = jwt.verify(jwtToken.substring(7), process.env.TOKEN_SECRET);
