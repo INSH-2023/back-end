2aecf9e91ea61d3beadf7c330776c001275c6fb2
Sahathat Yingsakulkiet <61311677+sahathat@users.noreply.github.com>
2023-08-11 10:02:56 +0700

    Revert "fix bug return wrong message"
    This reverts commit 51eaa0e450e1b276c278e5debf01268be872b387.

--- express/middleware/jwtAuthen.js
+++ express/middleware/jwtAuthen.js
@@ -12,7 +12,7 @@ exports.JwtAuth = (req, res, next) => {
   const jwtToken = "Bearer " + req.cookies.token;
   // const jwtRefreshToken = req.headers.refresh || "Bearer " + req.cookies.refreshToken ;
   // ตรวจสอบถ้าไม่มี token จะเข้าสู่ระบบไม่ได้
-  if ([null,undefined].includes(req.cookies.token)) return res.status(401).json(errorModel("need login first",req.originalUrl))
+  if (["null","undefined"].includes(jwtToken.substring(7))) return res.status(401).json(errorModel("need login first",req.originalUrl))
   try {
     // ตรวจสอบ user ใน access token 
     let user = jwt.verify(jwtToken.substring(7), process.env.TOKEN_SECRET);
