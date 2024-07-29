fffa19de7680a10fdf95236e2db43690e457a406
Sahathat Yingsakulkiet <61311677+sahathat@users.noreply.github.com>
2024-07-29 13:55:13 +0700

    Switched to ASP.NET repository

--- HelpDeskNet/Configures/MongoDBConfigure.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HelpDeskNet.Configures
-{
-    public class MongoDBConfigure
-    {
-        public string ConnectionURI { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
-        public string CollectionName { get; set; } = null!;
-    }
-}
